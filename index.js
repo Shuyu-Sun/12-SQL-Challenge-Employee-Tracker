c587958d6b46f765c4b5084b36ee41fe43a428b6
Stella Sun <sunshuyu0413@163.com>
2022-07-07 23:08:40 -0400

    add env file

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+//require dependencies
 const inquirer = require("inquirer");
+const mysql = require('mysql2');
+require("console.table");
+
+//mysql connection
+const connection = mysql.createConnection({
+
+  });  
+  
+  connection.connect((err) => {
+    if (err) throw err;
+  
+    runSearch();
+  });
